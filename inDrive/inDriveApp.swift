d22f5d8c8f50ec29738c45a58df6a040339f0d77
tolebishka <aitenovtolebi05@gmail.com>
2025-09-13 17:07:22 +0500

    Initial Commit

---
+++ inDrive/inDriveApp.swift
@@ -0,0 +1,17 @@
+//
+//  inDriveApp.swift
+//  inDrive
+//
+//  Created by Arcana Inc on 13.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct inDriveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
